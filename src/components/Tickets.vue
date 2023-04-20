a8b2409d2057a274e8539f3a27aa542da52456ed
Randy <monyerandy15@gmail.com>
2023-04-20 17:47:55 +0100

    Navigation Bar

--- src/components/Tickets.vue
+++ src/components/Tickets.vue
@@ -1,43 +1,51 @@
 <template>
+  <div>
+    <h1>Convinced</h1>
+    <p>Let's get you a spot now</p>
+
+    <!-- Tickets  -->
     <div>
-        <h1>Convinced</h1>
-        <p>Let's get you a spot now</p>
+      <!-- In-Person -->
+      <div>
+        <p>In-person</p>
+        <p>N1500</p>
 
-        <!-- Tickets  -->
+        <!-- Perks -->
+        <ul>
+          <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
+          <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
+          <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
+          <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
+          <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
+          <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
+          <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
+        </ul>
         <div>
-            <!-- In-Person -->
-            <div>
-                <p>In-person</p>
-                <p>N1500</p>
-
-                <!-- Perks -->
-                <ul>
-                    <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
-                    <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
-                    <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
-                    <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
-                    <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
-                    <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
-                    <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
-                </ul>
-            </div>
-
-            <!-- Online -->
-            <div>
-            <p>Online</p>
-            <p>Free</p>
-
-            <!-- Perks -->
-            <ul>
-                <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
-                <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
-                <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
-                <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
-                <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
-                <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
-                <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
-            </ul>
+          <a href="">Buy Tickets</a>
+          <img src="../assets/Images/arrow-link-purple.svg" alt="" />
         </div>
+      </div>
+
+      <!-- Online -->
+      <div>
+        <p>Online</p>
+        <p>Free</p>
+
+        <!-- Perks -->
+        <ul>
+          <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
+          <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
+          <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
+          <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
+          <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
+          <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
+          <li><span>&#10022; </span>Feature perk 1 for in person attendee</li>
+        </ul>
+        <div>
+          <a href="">Reserve your spot</a>
+          <img src="../assets/Images/arrow-link-blue.svg" alt="" />
         </div>
+      </div>
     </div>
+  </div>
 </template>
