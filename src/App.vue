9256bf23fbbe9733073359e739745e41e24f3ac9
Randy <monyerandy15@gmail.com>
2023-04-21 00:32:31 +0100

    Loading Page

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,43 @@
 <template>
-  <nav
-    class="w-full bg-white px-4 py-5 flex justify-between items-center fixed font-pry md:px-12"
-  >
-    <!-- Logo -->
-    <router-link to="/">
-      <img src="./assets/Images/Logo.svg" alt="Buy tickets" />
-    </router-link>
+  <Loading v-if="showLoadingPage" />
 
-    <!-- Main Links (Desktop) -->
-    <div class="hidden md:flex md:items-center md:gap-7 text-base font-extrabold leading-6">
-      <router-link to="/about">About</router-link>
-      <router-link to="/">Speakers</router-link>
-      <router-link to="/">Organizers</router-link>
-      <router-link to="/">Volunteer</router-link>
-      <router-link to="/about">Sponsors</router-link>
-
-      <router-link
-        to="/about"
-        class="flex items-center justify-center text-purple
-        uppercase underline "
-      >
-        <span class="mr-2">Buy tickets</span>
-        <img src="./assets/Images/arrow-link-purple.svg" alt="Arrow" class="w-3.5" />
+  <div v-else>
+    <nav
+      class="w-full bg-white px-4 py-5 flex justify-between items-center fixed font-pry md:px-12"
+    >
+      <!-- Logo -->
+      <router-link to="/">
+        <img src="./assets/Images/Logo.svg" alt="Buy tickets" />
       </router-link>
-    </div>
 
-    <!-- Hamburger Menu (Mobile) -->
+      <!-- Main Links (Desktop) -->
+      <div
+        class="hidden md:flex md:items-center md:gap-7 text-base font-medium leading-6"
+      >
+        <router-link to="/about">About</router-link>
+        <router-link to="/">Speakers</router-link>
+        <router-link to="/">Organizers</router-link>
+        <router-link to="/">Volunteer</router-link>
+        <router-link to="/about">Sponsors</router-link>
 
-    <div class="md:hidden flex items-center">
-      <!-- Buy Ticket link -->
-      <router-link to="/about" class="flex items-center mr-7 md:block">
-        <span
-          class="font-bold font-pry text-xs uppercase text-purple mr-2 mt-0.5"
-          >Buy tickets</span
+        <router-link
+          to="/about"
+          class="flex items-center justify-center text-purple uppercase underline"
         >
-        <div>
+          <span class="mr-2">Buy tickets</span>
           <img
             src="./assets/Images/arrow-link-purple.svg"
-            class="w-2.5"
             alt="Arrow"
+            class="w-3.5"
           />
-        </div>
-      </router-link>
+        </router-link>
+      </div>
 
-      <button class="focus:outline-none" @click="toggle">
-        <div class="w-6 h-0.5 bg-hamburgerColor mb-1.5"></div>
-        <div class="w-6 h-0.5 bg-hamburgerColor mb-1.5"></div>
-        <div class="w-6 h-0.5 bg-hamburgerColor"></div>
-      </button>
-    </div>
+      <!-- Hamburger Menu (Mobile) -->
 
-    <!-- Main Links (Mobile) -->
-
-    <div
-      class="flex flex-col absolute top-9 left-0 z-10 w-full md:w-1/2 bg-softBrown text-black hover:purple px-5 py-7 gap-6 mobile-menu mt-5 uppercase tracking-tighter font-medium text-sm leading-120"
-      :class="{ hidden: !showMobileMenu }"
-    >
-      <router-link to="/about">About</router-link>
-      <router-link to="/">Speakers</router-link>
-      <router-link to="/">Organizers</router-link>
-      <router-link to="/">Volunteer</router-link>
-      <router-link to="/about">Sponsors</router-link>
-
-      <div class="flex justify-between items-center space-between mt-2">
-        <router-link to="/about" class="flex items-center">
+      <div class="md:hidden flex items-center">
+        <!-- Buy Ticket link -->
+        <router-link to="/about" class="flex items-center mr-7 md:block">
           <span
             class="font-bold font-pry text-xs uppercase text-purple mr-2 mt-0.5"
             >Buy tickets</span
@@ -77,26 +51,89 @@
           </div>
         </router-link>
 
-        <div class="text-deepBrown font-bold text-xs leading-7" @click="toggle">
-          Close
+        <button class="focus:outline-none" @click="toggle">
+          <div class="w-6 h-0.5 bg-hamburgerColor mb-1.5"></div>
+          <div class="w-6 h-0.5 bg-hamburgerColor mb-1.5"></div>
+          <div class="w-6 h-0.5 bg-hamburgerColor"></div>
+        </button>
+      </div>
+
+      <!-- Main Links (Mobile) -->
+
+      <div
+        class="flex flex-col absolute top-9 left-0 z-10 w-full md:w-1/2 bg-softBrown text-black hover:purple px-5 py-7 gap-6 mobile-menu mt-5 uppercase tracking-tighter font-medium text-sm leading-120"
+        :class="{ hidden: !showMobileMenu }"
+      >
+        <router-link to="/about">About</router-link>
+        <router-link to="/">Speakers</router-link>
+        <router-link to="/">Organizers</router-link>
+        <router-link to="/">Volunteer</router-link>
+        <router-link to="/about">Sponsors</router-link>
+
+        <div class="flex justify-between items-center space-between mt-2">
+          <router-link to="/about" class="flex items-center">
+            <span
+              class="font-bold font-pry text-xs uppercase text-purple mr-2 mt-0.5"
+              >Buy tickets</span
+            >
+            <div>
+              <img
+                src="./assets/Images/arrow-link-purple.svg"
+                class="w-2.5"
+                alt="Arrow"
+              />
+            </div>
+          </router-link>
+
+          <div
+            class="text-deepBrown font-bold text-xs leading-7"
+            @click="toggle"
+          >
+            Close
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </div>
   <router-view></router-view>
 </template>
 
 <script>
+import Loading from "./components/Loading.vue";
 export default {
+  name: "App",
+  components: { Loading },
+
   data() {
     return {
       showMobileMenu: false,
+      showLoadingPage: !localStorage.getItem("visited"),
     };
   },
   methods: {
     toggle() {
       this.showMobileMenu = !this.showMobileMenu;
     },
+  },
+  mounted() {
+    // Show loading page for 5 seconds
+    if (!localStorage.getItem("visited")) {
+      localStorage.setItem("visited", true);
+      setTimeout(() => {
+        this.showLoadingPage = false;
+        this.$router.replace({ name: "Home" });
+      }, 8000);
+    } else {
+      this.showLoadingPage = false;
+    }
+
+    // Hide scrollbar and disable scrolling
+    document.body.style.overflow = "hidden";
+
+    // Restore scrollbar and scrolling after 5 seconds
+    setTimeout(() => {
+      document.body.style.overflow = "";
+    }, 8000);
   },
 };
 </script>
@@ -122,6 +159,3 @@ export default {
   }
 }
 </style>
-
-TODO: // Align hamburger wit logo Font // Main links, font bg color on mobile
-adjust arrow Reduce side margins on mobile
---
+++ src/components/Loading.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen bg-purple">
+    <div>
+      <h1
+        class="font-pry font-bold text-white text-2xl lg:text-6xl lg:font-bold lg:tracking-wider"
+      >
+        Influencers Meet Tech <br />
+        is <span class="text-pink opacity-10">back.</span>
+      </h1>
+      <div class="loading-container relative mt-6">
+        <div class="loading-bar" :style="{ width: `${progress * 100}%` }"></div>
+        <p
+          class="loading-text font-sec font-semibold lg:font-semibold lg:text-2xl"
+        >
+          Loading {{ progress.toFixed(2) }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      progress: 0,
+    };
+  },
+  mounted() {
+    const endTime = Date.now() + 8000;
+
+    const intervalId = setInterval(() => {
+      const now = Date.now();
+      const timeLeft = endTime - now;
+      const percentageLeft = timeLeft / 8000;
+      this.progress = 1 - percentageLeft;
+
+      if (timeLeft <= 0) {
+        clearInterval(intervalId);
+      }
+    }, 10);
+  },
+};
+</script>
+
+<style>
+.loading-container {
+  min-width: 32px;
+  width: 70vw;
+  height: 52px; 
+  max-width: 40rem;
+  position: relative;
+  background-color: rgba(255, 255, 255, 0.1);
+}
+
+.loading-bar {
+  position: absolute;
+  top: 0; 
+  left: 0;
+  transform: translateY(0);
+  height: 100%; 
+  background-color: white;
+  animation: loading-animation 8s linear forwards;
+  z-index: 1;
+}
+
+.loading-text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+  color: black;
+}
+
+@media (max-width: 768px) {
+  .loading-text {
+    font-size: 1rem;
+  }
+}
+
+@media (max-width: 576px) {
+  .loading-container {
+    width: 90vw;
+    max-width: unset;
+  }
+
+  .loading-text {
+    font-size: 0.8rem;
+  }
+}
+
+@keyframes loading-animation {
+  0% {
+    width: 0;
+  }
+  100% {
+    width: 100%;
+  }
+}
+</style>
